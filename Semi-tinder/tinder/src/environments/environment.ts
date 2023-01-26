096d195ba360ce71b2400ffdbd4219e517302e75
ojtdev <jandenielbondocbondoc@gmail.com>
2023-01-26 17:40:31 +0800

    First Commit

---
+++ Semi-tinder/tinder/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-sign-up-b1efa',
+    appId: '1:650398659801:web:5243d448cee12857efbac8',
+    storageBucket: 'angular-sign-up-b1efa.appspot.com',
+    apiKey: 'AIzaSyBoYzOktbR78l7ku6BdLqyO5j9bd1SdnC4',
+    authDomain: 'angular-sign-up-b1efa.firebaseapp.com',
+    messagingSenderId: '650398659801',
+  },};
